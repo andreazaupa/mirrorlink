04f6dbc2c0bfe49914328428d34841620be2356d
Andrea Zaupa <andrea.zaupa@develon.com>
2011-10-21 10:26:55 +0200

    new redirect

--- app/controllers/mirrors_controller.rb
+++ app/controllers/mirrors_controller.rb
@@ -4,7 +4,7 @@ class MirrorsController < ApplicationController
   # GET /mirrors/1
   # GET /mirrors/1.json
   def show
-    @mirror = Mirror.find(params[:id])
+    @mirror = Mirror.find_by_id(params[:id])
     @mirror ||= Mirror.find(:all,:conditions=>["slug = ?",params[:id]]).first
     headers["Status"] = "301 Moved Permanently"
     redirect_to @mirror.uri
